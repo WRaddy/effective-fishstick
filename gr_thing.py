b5f8c6710069016d4f963ead5b52fa214b8abd68
WRaddy <w.r.a.d.d.y.w@gmail.com>
2025-04-06 19:34:47 +0100

    initial stuff

--- gr_thing.py
+++ gr_thing.py
@@ -2,8 +2,13 @@ import pytesseract
 from PIL import Image
 import pyautogui
 import discord
+from dotenv import load_dotenv
+import os
 import io
 from time import sleep
+
+# Load environment variables from .env file
+load_dotenv()
 
 sleep(3)
 screenshot = pyautogui.screenshot()
@@ -17,10 +22,12 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'Logged in as {client.user}')
     
-    channel = client.get_channel(1358501640659013776)  
+    # Get channel ID and bot token from environment variables
+    channel_id = int(os.getenv('CHANNEL_ID'))  # Convert to int
+    channel = client.get_channel(channel_id)  
     if channel:
         await channel.send(f"Here's the OCR output:\n\n{text}")
     await client.close()
 
-# Run the bot
-client.run('MTI4NTM1NDgwMDAzMjEyMDg2Mw.GZ7QWe.TwNS0qCoQG805jimYdiAoozKwK--Y0jGP-O1UA')
+# Run the bot using the token from the .env file
+client.run(os.getenv('DISCORD_TOKEN'))
